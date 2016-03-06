eaf1c9c249cedd3d352e487f1c40bc9d2708b7c1
Karolina Rostek <karolina.rostek.93@gmail.com>
2016-03-06 16:08:46 +0100

    Usuwanie folder

--- projekt1/Zadanie2/src/main/java/mavenTest/Zadanie23/NieudanyPsikusException.java
+++
@@ -1,5 +0,0 @@
-package mavenTest.Zadanie23;
-
-public class NieudanyPsikusException extends Exception {
-	private static final long serialVersionUID = 1L;
-}
--- projekt1/Zadanie2/src/main/java/mavenTest/Zadanie23/Psikus.java
+++
@@ -1,9 +0,0 @@
-package mavenTest.Zadanie23;
-
-public interface Psikus {
-	Integer CyfroKrad(Integer liczba);
-	Integer HultajChochla(Integer liczba) throws NieudanyPsikusException;
-	Integer Nieksztaltek (Integer liczba);
-	Integer Heheszki(Integer liczba);
-	boolean Titit(Integer liczb_dziel);
-}
