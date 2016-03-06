aa1ea0f3444c63577b1b0d774447201c75f5af11
Karolina Rostek <karolina.rostek.93@gmail.com>
2016-03-06 22:39:00 +0100

    Dodanie metody i testow Nieksztaltek

--- projekt1/Zadanie2/src/main/java/mavenTest/Zadanie23/Kontrakt.java
+++ projekt1/Zadanie2/src/main/java/mavenTest/Zadanie23/Kontrakt.java
@@ -16,13 +16,43 @@ public class Kontrakt implements Psikus {
 	}
 
 	public Integer Nieksztaltek(Integer liczba) {
-		// TODO Auto-generated method stub
-		return null;
+		int ileCyfrDoZamiany = 0;
+		for (char cyfra : liczba.toString().toCharArray()) {
+			if (cyfra == '3' || cyfra == '6' || cyfra == '7')
+				ileCyfrDoZamiany++;
+		}
+		if (ileCyfrDoZamiany == 0)
+			return liczba;
+		Random losowy = new Random();
+		int doZamiany = losowy.nextInt(ileCyfrDoZamiany);
+		String wynik = "";
+		int pomoc = 0;
+		for (int i = 0; i < liczba.toString().length(); i++) {
+			char cyfra = liczba.toString().charAt(i);
+			if (cyfra == '3' || cyfra == '6' || cyfra == '7') {
+				if (pomoc == doZamiany) {
+					switch (cyfra) {
+					case '3':
+						cyfra = '8';
+						break;
+					case '7':
+						cyfra = '1';
+						break;
+					case '6':
+						cyfra = '9';
+						break;
+					}
+				}
+				pomoc++;
+			}
+			wynik += cyfra;
+		}
+		return Integer.parseInt(wynik);
 	}
 
-	//zamienia liczbę na dowolną całkowitą z przedziału <0,liczba)
+	// zamienia liczbę na dowolną całkowitą z przedziału <0,liczba)
 	public Integer Heheszki(Integer liczba) {
-		if(liczba <= 0){
+		if (liczba <= 0) {
 			throw new IllegalArgumentException();
 		} else {
 			Random losowy = new Random();
@@ -31,15 +61,15 @@ public class Kontrakt implements Psikus {
 		}
 	}
 
-	//sprawdza, czy liczba jest podzielna przez liczba_dziel.
+	// sprawdza, czy liczba jest podzielna przez liczba_dziel.
 	public boolean Titit(Integer liczba_dziel) {
-		if(liczba_dziel == 0){
+		if (liczba_dziel == 0) {
 			throw new IllegalArgumentException();
 		} else {
-			if(liczba % liczba_dziel == 0 ){
+			if (liczba % liczba_dziel == 0) {
 				return true;
 			} else {
-				return false;	
+				return false;
 			}
 		}
 	}
--- projekt1/Zadanie2/src/test/java/mavenTest/Zadanie23/KontraktTest.java
+++ projekt1/Zadanie2/src/test/java/mavenTest/Zadanie23/KontraktTest.java
@@ -1,15 +1,16 @@
 package mavenTest.Zadanie23;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThan;
 
-import java.util.Arrays;
-import java.util.Collection;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.*;
 
 public class KontraktTest {
 	private Kontrakt kontrakt;
@@ -24,19 +25,38 @@ public class KontraktTest {
 	public void tearDown() throws Exception {
 		kontrakt = null;
 	}
-	
+
 	@Test
-	public void HeheszkiRandomDzialaTest(){
-		assertThat(kontrakt.Heheszki(5), greaterThanOrEqualTo(0));
+	public void czyZwrociArgumentTest() {
+		int wartosc = kontrakt.Nieksztaltek(124);
+		assertThat(wartosc, is(124));
 	}
-	
+
+	@Test
+	public void czyZamieniliczbe3Test() {
+		int wartosc = kontrakt.Nieksztaltek(453);
+		assertThat(wartosc, is(458));
+	}
+
+	@Test
+	public void czyZamieniliczbe7Test() {
+		int wartosc = kontrakt.Nieksztaltek(874);
+		assertThat(wartosc, is(814));
+	}
+
+	@Test
+	public void czyZamieniliczbe6Test() {
+		int wartosc = kontrakt.Nieksztaltek(624);
+		assertThat(wartosc, is(924));
+	}
+
 	@Test(expected = IllegalArgumentException.class)
-	public void HeheszkiMniejszeNizZeroTest(){
+	public void HeheszkiMniejszeNizZeroTest() {
 		kontrakt.Heheszki(-3);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
-	public void HeheszkiRowneZeroTest(){
+	public void HeheszkiRowneZeroTest() {
 		kontrakt.Heheszki(0);
 	}
 
@@ -44,14 +64,14 @@ public class KontraktTest {
 	public void TititLiczbaDzielJestZeremTest() {
 		kontrakt.Titit(0);
 	}
-	
+
 	@Test
-	public void TititJestPodzielneTest(){
-		assertTrue(kontrakt.Titit(5));	
+	public void TititJestPodzielneTest() {
+		assertTrue(kontrakt.Titit(5));
 	}
-	
+
 	@Test
-	public void TititNieJestPodzielneTest(){
+	public void TititNieJestPodzielneTest() {
 		assertFalse(kontrakt.Titit(10));
 	}
 }
