352f62b5a60eee64689e5c069a7b8b55b4779ab1
Karolina Rostek <karolina.rostek.93@gmail.com>
2016-03-06 11:57:52 +0100

    Dodanie pliku src

---
+++ projekt1/Zadanie1/src/main/java/mavenTest/Zadanie31/App.java
@@ -0,0 +1,13 @@
+package mavenTest.Zadanie31;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ projekt1/Zadanie1/src/main/java/mavenTest/Zadanie31/MyStack.java
@@ -0,0 +1,48 @@
+/*
+Utwórz projekt MAVEN z zadania 2 labolatorium 2 (klasa stos). 
+Stos ma być reprezentowany poprzez Listę. 
+Dla prostoty wrzucamy liczby całkowite.
+Rozpatrz następujące przypadki:
+1. Jaka jest wartość IsEmpty() na początku utworzenia stosu?
+2. Metodą push włóż element, następnie metodą top sprawdź wiele razy, czy wyszedł ten samelement.
+3. Poprawność działania metody pop.
+4. Wrzucenie wartości największej i najmniejszej na stosie.
+5. I inne ...
+ */
+
+package mavenTest.Zadanie31;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyStack {
+	private List<Integer> stack;
+
+	public MyStack() {
+		stack = new ArrayList<Integer>();
+	}
+
+	public boolean IsEmpty() {
+		return stack.size() == 0;
+	}
+
+	public void MyPop() {
+		if (stack.isEmpty()) {
+			throw new NullPointerException();
+		} else {
+			stack.remove(stack.size() - 1);
+		}
+	}
+
+	public void MyPush(int elem) {
+		stack.add(elem);
+	}
+
+	public int MyTop() {
+		if (stack.isEmpty()) {
+			throw new NullPointerException();
+		}
+		return stack.get(stack.size() - 1);
+	}
+
+}
