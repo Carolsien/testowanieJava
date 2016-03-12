1fbc20f5b95a7f14425ac029be5eeeeeb8e67e0f
Karolina Rostek <karolina.rostek.93@gmail.com>
2016-03-12 20:49:37 +0100

    usuniecie folderu MockitoExample

--- laboratorium4/MockitoExample/src/PersonDatabaseService.java
+++
@@ -1,8 +0,0 @@
-import java.sql.SQLException;
-
-public interface PersonDatabaseService {
-	int addPerson(Person person) throws SQLException;
-	int remove(int id) throws SQLException;
-	Person read(int id) throws SQLException;
-	int update(int id) throws SQLException;
-}
--- laboratorium4/MockitoExample/src/PersonMockDatabaseService.java
+++
@@ -1,7 +0,0 @@
-public interface PersonMockDatabaseService {
-  boolean addPerson(Person person) throws PersonException;
-  boolean remove(int id) throws PersonException;
-  boolean read(int id) throws PersonException;
-  boolean update(int id) throws PersonException;
-}
-
