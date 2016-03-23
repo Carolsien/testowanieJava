404843a5ea7fb02525924f9172145d7958c58761
Karolina Rostek <karolina.rostek.93@gmail.com>
2016-03-23 20:07:36 +0100

    dodanie pliku Messenger.java

--- projekt2/messenger/src/main/java/com/example/mockdemo/app/Messenger.java
+++ projekt2/messenger/src/main/java/com/example/mockdemo/app/Messenger.java
@@ -12,18 +12,33 @@ public class Messenger {
 		this.ms = ms;
 	}
 
-	/*Tymczasowo -> Pozniej to zniknie */
-	public Messenger() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public int testConnection(String server) {
+		switch (ms.checkConnection(server)) {
+		case FAILURE:
+			return 1;
+		case SUCCESS:
+			return 0;
+		}
 		return 1;
 	}
 
 	public int sendMessage(String server, String message) {
-
 		int result = -1;
+		try {
+			SendingStatus sStatus = ms.send(server, message);
+			switch (sStatus) {
+			case SENT:
+				result = 0;
+				break;
+			case SENDING_ERROR:
+				result = 1;
+				break;
+			default:
+				result = -1;
+			}
+		} catch (MalformedRecipientException e) {
+			result = 2;
+		}
 		return result;
 	}
 }
